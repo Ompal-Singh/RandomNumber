87102a58fa60103399b4e33bee3e3382064d58e2
Ompal Singh <ompal.singh_cs18@gla.ac.in>
2021-02-19 07:49:00 -0800

    print any random number

---
+++ src/RandomNumberExample1.java
@@ -0,0 +1,20 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Ompal Singh (Ompal-Singh)
+ *   Date: 2/19/2021
+ *   Time: 7:37 AM
+ *   File: RandomNumberExample1.java
+ */
+import java.lang.Math;
+public class RandomNumberExample1 {
+    public static void main(String args[])
+    {
+        //generating random numbers
+
+        System.out.println("1st Random Number:" + Math.random());
+        System.out.println("2nd Random Number:" + Math.random());
+        System.out.println("3rd Random Number:" + Math.random());
+        System.out.println("4th Random Number:" + Math.random());
+
+    }
+
+}
